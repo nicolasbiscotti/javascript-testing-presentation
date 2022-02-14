9a893e7b3cada96dff7dd3c1e92d9561f09e677c
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-02-14 09:34:23 -0300

    test(test/test_subtract.js): an exception to subtract funtion and it is checked

--- start/1.node/subtract.js
+++ start/1.node/subtract.js
@@ -1,4 +1,9 @@
 export default function subtract(x, y, cb) {
   let answer = x - y;
+  if (answer < 0) {
+    const err = new Error("result is negative");
+    err.code = 12345
+    cb(err, null);
+  }
   cb(null, answer);
 }
--- start/1.node/test/test_subtract.js
+++ start/1.node/test/test_subtract.js
@@ -17,4 +17,20 @@ describe("subtract", () => {
       done();
     });
   });
+
+  it("shoul return an error when subtract 3 from 2", (done) => {
+    // arrange
+    const x = 2;
+    const y = 3;
+    const expected = 12345;
+
+    // act
+    subtract(x, y, (error, actual) => {
+      // assert
+      expect(actual).to.equal(null);
+      expect(error.code).to.equal(expected);
+
+      done();
+    });
+  });
 });

3bec2273f4511445794dcc9892ae20c935d67614
Nicolás Biscotti <nicolas.biscotti@gmail.com>
2022-02-15 09:03:40 -0300

    test(test/test_multiply.js): async test

---
+++ start/1.node/test/test_multiply.js
@@ -0,0 +1,45 @@
+import { expect } from "chai";
+import multiply from "../multiply.js";
+
+describe("multiply", () => {
+  it("shoul multiply 2 and 3", (done) => {
+    // arrange
+    const x = 2;
+    const y = 3;
+    const expected = 6;
+
+    // act
+    multiply(x, y).then((actual) => {
+      // assert
+      expect(actual).to.equal(expected);
+
+      done();
+    });
+  });
+
+  it("shoul multiply 4 and 3", () => {
+    // arrange
+    const x = 4;
+    const y = 3;
+    const expected = 12;
+
+    // act
+    return multiply(x, y).then((actual) => {
+      // assert
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  it("shoul multiply 4 and 5", async () => {
+    // arrange
+    const x = 4;
+    const y = 5;
+    const expected = 20;
+
+    // act
+    const actual = await multiply(x, y);
+
+    // assert
+    expect(actual).to.equal(expected);
+  });
+});
